704af8980ca90e4b5e5eb65df59acea0820f2dee
violencedev <furkanesen39@gmail.com>
2022-01-22 17:32:27 +0300

    admin-system
    admin-system resource has been created and started being developed.
    
    Co-Authored-By: Mert AHMETOĞLU <64382208+asikLEMMY@users.noreply.github.com>

---
+++ mods/deathmatch/resources/admins/back-end/staff_integration.lua
@@ -0,0 +1,11 @@
+adminLevels = {
+    [0] = "Para Babası",
+    [1] = "Çeyrek Rehber",
+    [2] = "Yarı Rehber",
+    [3] = "Tam Rehber",
+    [4] = "Çeyrek Admin",
+    [5] = "Yarı Admin",
+    [6] = "Tam Admin",
+    [7] = "Geliştirici Herif",
+    [8] = "Parahan Kurucu"
+}
